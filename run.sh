9202544cfcff19ed3aae864bf8d28b698d42df3d
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-05 20:11:49 -0300

    setando online mode false

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 docker run -d --name $1 \
              -v $2:/data \
-	        -e EULA=true -e TYPE=BUKKIT -e VERSION=$3 -e MEMORY=$4 \
+	        -e EULA=true -e TYPE=BUKKIT -e VERSION=$3 -e MEMORY=$4 -e ONLINE_MODE=FALSE \
 	        -p 25565:25565 \
             $5
   

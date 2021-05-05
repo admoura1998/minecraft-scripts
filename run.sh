5546d7e4206865744f1d51045d5e678ad72f00e4
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-05 16:31:56 -0300

    removendo if pos docker run

--- run.sh
+++ run.sh
@@ -5,11 +5,7 @@ docker run -d --name $1 \
 	        -e EULA=true -e TYPE=BUKKIT -e VERSION=$3 -e MEMORY=$4 \
 	        -p 25565:25565 \
             $5
-
-if [ $? -eq 1 ];
-then
-    exit 1
-fi    
+  
 
 
 

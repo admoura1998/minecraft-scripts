7bc7372769bfab082fb90d1c031aebaf7a88196e
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-05 16:25:47 -0300

    voltando com tag -d

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --name $1 \
+docker run -d --name $1 \
              -v $2:/data \
 	        -e EULA=true -e TYPE=BUKKIT -e VERSION=$3 -e MEMORY=$4 \
 	        -p 25565:25565 \

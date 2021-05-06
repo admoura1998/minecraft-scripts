ed03c60439b98ea552e44c6c3170ee06f4cb9ebb
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-05 22:39:42 -0300

    trocando variavel de posicao

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 docker run -d --name $1 \
-             -v $2:/data \
-	        -e EULA=true -e TYPE=BUKKIT -e VERSION=$3 -e MEMORY=$4 -e ONLINE_MODE=FALSE \            
-	        -p $5 \
+            -v $2:/data \
+	        -e EULA=true -e TYPE=BUKKIT -e VERSION=$3 \
+            -e MEMORY=$4 -e ONLINE_MODE=FALSE \
+            -p $5 \
             $6
   
 

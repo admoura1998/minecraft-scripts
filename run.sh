0a122a556c230060ca7e84c5107772631f75c7ed
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-05 22:35:54 -0300

    consertando -p

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 docker run -d --name $1 \
              -v $2:/data \
 	        -e EULA=true -e TYPE=BUKKIT -e VERSION=$3 -e MEMORY=$4 -e ONLINE_MODE=FALSE \            
-	        -p $6:$6 \
+	        -p "$6:$6" \
             $5
   
 

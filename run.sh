57aa6e1b2a35c1b7d82b30117d9de5d73bcc092d
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-05 22:28:43 -0300

    add query port

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 docker run -d --name $1 \
              -v $2:/data \
 	        -e EULA=true -e TYPE=BUKKIT -e VERSION=$3 -e MEMORY=$4 -e ONLINE_MODE=FALSE \
-	        -p $6 \
+            -e ENABLE_QUERY=true -e QUERY_PORT=$6 \
+	        -p $6:$6 \
             $5
   
 

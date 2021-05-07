17c43ca01ce6c18178e4441ec66ce1a0b2786f6b
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-07 15:14:20 -0300

    ajeitando parametro de porta

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ docker run -d --name $1 \
             -v $2:/data \
 	        -e EULA=true -e TYPE=BUKKIT -e VERSION=$3 \
             -e MEMORY=$4 -e ONLINE_MODE=FALSE \
-            -p $5 \
+            -p $5:$5 \
             $6
   
 

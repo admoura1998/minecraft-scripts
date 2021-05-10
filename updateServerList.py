69aae31db81a84d10d98b53b10651c244a25ab54
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-10 12:00:58 -0300

    updateServerList

--- updateServerList.py
+++ updateServerList.py
@@ -1,22 +1,21 @@
 import os
 import re
+import fileinput
+import sys
 
-# file = os.getenv('SERVER_LIST_PATH')+"/"+os.getenv('SERVER_LIST_FILE')
-# server = os.getenv('NOME_CONTAINER')
+arch = os.getenv('SERVER_LIST_PATH')+"/"+os.getenv('SERVER_LIST_FILE')
+server = os.getenv('NOME_CONTAINER')
 
-arch="teste.txt"
-line="servers="
-server="mserver"
-
-def changeServerFile():    
+def updateServerFile(server):    
     with open(arch, 'r+') as file: 
         data = file.read()                                           
         newData = re.sub(r"^{}".format(line),line+server+',', data, flags=re.M)         
     with open(arch, 'w') as file:
-        file.write(newData)                                                    
+        file.write(newData)  
 
 def main():
-    changeServerFile()
+    updateServerFile(server)
+    
 
 if __name__ == "__main__":
     main()

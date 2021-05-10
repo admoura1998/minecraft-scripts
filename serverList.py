9240a81ce8a9b1f1c6cebbf3a0d44da555d3d0ed
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-10 10:29:17 -0300

    consertando aspas

--- serverList.py
+++ serverList.py
@@ -1,12 +1,12 @@
 import os
 import re
 
-file = os.getenv("SERVER_LIST_PATH")+"/"+os.getenv("SERVER_LIST_FILE")
+file = os.getenv('SERVER_LIST_PATH')+"/"+os.getenv('SERVER_LIST_FILE')
 
 with open(file) as file:
     print("lele")
 
-    
+
 
 
 

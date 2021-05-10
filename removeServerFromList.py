ba89bdb3c847ea5773b0e470731e9f957e64df91
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-10 13:59:30 -0300

    mudando nome do script

---
+++ removeServerFromList.py
@@ -0,0 +1,18 @@
+import os
+import re
+import fileinput
+import sys
+
+arch = os.getenv('SERVER_LIST_PATH')+"/"+os.getenv('SERVER_LIST_FILE')
+server = os.getenv('SERVIDOR')
+
+def removeServer(server):
+    for line in fileinput.input(arch, inplace=1):
+        sys.stdout.write(line.replace(server+",", ""))
+
+
+def main():    
+   removeServer(server)
+
+if __name__ == "__main__":
+    main()

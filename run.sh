5388b52e04b7f083565975f9ae606b90a39afa14
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-10 19:13:49 -0300

    mudando porta rcon

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ docker run -d --name $1 \
             -v $2:/data \
 	        -e EULA=true -e TYPE=BUKKIT -e VERSION=$3 \
             -e MEMORY=$4 -e ONLINE_MODE=FALSE -e ENABLE_RCON=true -e RCON_PASSWORD=teste \
-            -e RCON_PORT=28016
+            -e RCON_PORT=28016 \
             -p $5:$5 \
             $6
   

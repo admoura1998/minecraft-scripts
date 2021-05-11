3ad31aa7a10556b2e067d675f08207506383e744
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-10 21:55:53 -0300

    adicionando parametro no tipo do servidor

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 docker run -d --name $1 \
             -v $2:/data \
-	        -e EULA=true -e TYPE=BUKKIT -e VERSION=$4 \
+	        -e EULA=true -e TYPE=$3 -e VERSION=$4 \
             -e MEMORY=$5 -e ONLINE_MODE=FALSE -e ENABLE_RCON=TRUE \
              -e RCON_PASSWORD=teste -e RCON_PORT=28016 \
             -p $6:$6 \

9ca3a97ba6a6ab0b4ee2950bb2eae3df27f79edd
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-05-30 18:18:58 -0300

    revertando porta

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ docker run -d --name $1 \
 	        -e EULA=true -e TYPE=$3 -e VERSION=$4 -e DIFFICULTY=$5 \
             -e MEMORY=$6 -e ONLINE_MODE=FALSE -e ENABLE_RCON=TRUE \
             -e RCON_PASSWORD=teste -e RCON_PORT=28016 -e SPAWN_PROTECTION=0 \
-            -p $7:$7 \
+            -p $7:25565 \
             $8
   
 

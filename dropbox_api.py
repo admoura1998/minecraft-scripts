ae420db4a891b478f42eb4ce34458d6212b30afc
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-06-04 00:58:50 -0300

    mudando exit de lugar

--- dropbox_api.py
+++ dropbox_api.py
@@ -8,22 +8,22 @@ opt = os.getenv("OPERATION")
 def upload_file(dbx, file, destination):
     with open(file, "rb+") as f:
         try:
-            dbx.files_upload(f.read(), f"/{destination}.{file.split('.')[1]}", mode=dropbox.files.WriteMode.overwrite)
-            exit(0)
+            dbx.files_upload(f.read(), f"/{destination}.{file.split('.')[1]}", mode=dropbox.files.WriteMode.overwrite)            
         except dropbox.exceptions.ApiError as err:
-            print(err)  
+            print(err)
+            exit(0)  
 
 
 def download_file(dbx,host_path,path):
     try: 
-        file = dbx.files_download_to_file(host_path+'/'+path.split('/')[1],path)
-        exit(0)
+        file = dbx.files_download_to_file(host_path+'/'+path.split('/')[1],path)       
     except dropbox.exceptions.ApiError as err:
         print(err)
+        exit(0)
 
 
 if(opt == 0):
-    upload_file(dbx, os.getenv("FILE"), os.getenv("DESTINATION"))
+    upload_file(dbx, os.getenv("FILE"), os.getenv("DESTINATION"))    
 download_file(dbx, os.getenv("HOST_PATH"), os.getenv("PATH"))                        
     
 

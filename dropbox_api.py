590c8a19b00104014d751bdd8044731090c0bb91
admoura1998 <alvaro.moura@infoglobo.com.br>
2021-06-04 01:35:25 -0300

    limpando codigo

--- dropbox_api.py
+++ dropbox_api.py
@@ -6,31 +6,32 @@ dbx = dropbox.Dropbox(oauth2_access_token=TOKEN, app_key="n56k7vj4k8uwer3")
 
 print(f"DBX -> {dbx}")
 
-opt = os.getenv("OPERATION")
+opt = int(os.getenv("OPERATION"))
 file = os.getenv("FILE")
 destination = os.getenv("DESTINATION")
 host_path = os.getenv("HOST_PATH")
 path = os.getenv("PATH")
-opt = int(opt)
 
 def upload_file(dbx, file, destination):
-    with open(file, "rb+") as f:        
-        dbx.files_upload(f.read(), f"/{destination}.{file.split('.')[1]}", mode=dropbox.files.WriteMode.overwrite) 
-                     
-        
+    with open(file, "rb+") as f: 
+        try:       
+            dbx.files_upload(f.read(), f"/{destination}.{file.split('.')[1]}", mode=dropbox.files.WriteMode.overwrite)
+            exit(0)
+        except dropbox.exceptions.ApiError as err:
+            print(err)
+            exit(1)
 
-def download_file(dbx,host_path,path):    
-    file = dbx.files_download_to_file(host_path+'/'+path.split('/')[1],path)       
-          
-
-print(f"OPERTATION -> {opt}")
-print(f"FILE -> {file} ")
-print(f"DESTINATION -> {destination} ")
+def download_file(dbx,host_path,path): 
+    try:   
+        file = dbx.files_download_to_file(host_path+'/'+path.split('/')[1],path)       
+    except dropbox.exceptions.ApiError as err:
+            print(err)
+            exit(1) 
 
 if opt == 0:
     upload_file(dbx, file, destination)
-    exit(0)    
-#download_file(dbx, host_path, path)                        
+download_file(dbx, host_path, path)   
+                      
     
 
 
